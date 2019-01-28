81e171b9f5a61a67838386f2bd9cd3293dd48c27
Mateusz <thoorson@gmail.com>
2019-01-28 23:22:39 +0100

    javascript hamburger, css menu mobile

---
+++ dist/js/main.js
@@ -0,0 +1,32 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuMobileContainer = document.querySelector(".menu-mobile-container");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+
+// set state of menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuMobileContainer.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+
+    // set menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuMobileContainer.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+
+    // set menu state
+    showMenu = false;
+  }
+}
