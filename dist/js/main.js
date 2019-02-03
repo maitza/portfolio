93c6963179a49a7f6e3d3c7c1a690469f3c2ba72
Mateusz <thoorson@gmail.com>
2019-02-03 20:33:34 +0100

    fixed menu links, added js to hamburger & menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,13 +8,13 @@ const navLink = document.querySelectorAll(".nav-link");
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
+menuNav.addEventListener("click", showActive);
 
 function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
     menu.classList.add("show");
     menuNav.classList.add("show");
-    //menuMobileContainer.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
 
     // set menu state
@@ -23,11 +23,25 @@ function toggleMenu() {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
-    //menuMobileContainer.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
 
     // set menu state
     showMenu = false;
+  }
+}
+
+// adding css to active li - menu link
+// Loop through the buttons and add the active class to the current/clicked button
+
+function showActive() {
+  for (let i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener("click", function() {
+      let current = document.getElementsByClassName("current");
+      current[0].className = current[0].className.replace(" current", "");
+      this.className += " current";
+    });
+    // closing menu after link click
+    toggleMenu();
   }
 }
 
