c33b5f25cf562b7cac346b3f6905e0914b3ef0f2
Mateusz <thoorson@gmail.com>
2019-02-03 15:19:51 +0100

    working with BEM css, repaired flexbox menu & hamburger

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,5 @@
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-const menuMobileContainer = document.querySelector(".menu-mobile-container");
+const menuBtn = document.querySelector(".container_hamburger");
+const menu = document.querySelector(".container__nav");
 const menuNav = document.querySelector(".menu-nav");
 const navItems = document.querySelectorAll(".nav-item");
 const navLink = document.querySelectorAll(".nav-link");
@@ -15,7 +14,7 @@ function toggleMenu() {
     menuBtn.classList.add("close");
     menu.classList.add("show");
     menuNav.classList.add("show");
-    menuMobileContainer.classList.add("show");
+    //menuMobileContainer.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
 
     // set menu state
@@ -24,7 +23,7 @@ function toggleMenu() {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
-    menuMobileContainer.classList.remove("show");
+    //menuMobileContainer.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
 
     // set menu state
