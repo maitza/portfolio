357d7e3b91d2bfe3400ff6cf8c7aaf652ba9231c
Mateusz <thoorson@gmail.com>
2019-05-09 20:10:57 +0200

    fixed drop down menu, addes some JS - active links

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,6 +9,7 @@ let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
 menuNav.addEventListener("click", showActive);
+window.addEventListener("scroll", hideMenu);
 
 function toggleMenu() {
   if (!showMenu) {
@@ -20,6 +21,20 @@ function toggleMenu() {
     // set menu state
     showMenu = true;
   } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+
+    // set menu state
+    showMenu = false;
+  }
+}
+
+// hiding menu when you scroll page
+
+function hideMenu() {
+  if (showMenu === true) {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
@@ -44,3 +59,60 @@ function showActive() {
     toggleMenu();
   }
 }
+
+// active link change id on scroll
+// Cache selectors
+var lastId,
+  topMenu = $(".menu-nav"),
+  topMenuHeight = topMenu.outerHeight() + 15,
+  // All list items
+  menuItems = topMenu.find("a"),
+  // Anchors corresponding to menu items
+  scrollItems = menuItems.map(function() {
+    var item = $($(this).attr("href"));
+    if (item.length) {
+      return item;
+    }
+  });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e) {
+  var href = $(this).attr("href"),
+    offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+  $("html, body")
+    .stop()
+    .animate(
+      {
+        scrollTop: offsetTop
+      },
+      300
+    );
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function() {
+  // Get container scroll position
+  var fromTop = $(this).scrollTop() + topMenuHeight;
+
+  // Get id of current scroll item
+  var cur = scrollItems.map(function() {
+    if ($(this).offset().top < fromTop) return this;
+  });
+  // Get the id of the current element
+  cur = cur[cur.length - 1];
+  var id = cur && cur.length ? cur[0].id : "";
+
+  if (lastId !== id) {
+    lastId = id;
+    // Set/remove active class
+    menuItems
+      .parent()
+      .removeClass("current")
+      .end()
+      .filter("[href='#" + id + "']")
+      .parent()
+      .addClass("current");
+  }
+});
