468018923d45ff5d96b6a5d40fb1a388b12cef69
Mateusz <thoorson@gmail.com>
2019-05-11 13:26:17 +0200

    added backToTop button - html/css + js

--- dist/js/main.js
+++ dist/js/main.js
@@ -62,14 +62,14 @@ function showActive() {
 
 // active link change id on scroll
 // Cache selectors
-var lastId,
+let lastId,
   topMenu = $(".menu-nav"),
   topMenuHeight = topMenu.outerHeight() + 15,
   // All list items
   menuItems = topMenu.find("a"),
   // Anchors corresponding to menu items
   scrollItems = menuItems.map(function() {
-    var item = $($(this).attr("href"));
+    let item = $($(this).attr("href"));
     if (item.length) {
       return item;
     }
@@ -78,7 +78,7 @@ var lastId,
 // Bind click handler to menu items
 // so we can get a fancy scroll animation
 menuItems.click(function(e) {
-  var href = $(this).attr("href"),
+  let href = $(this).attr("href"),
     offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
   $("html, body")
     .stop()
@@ -94,15 +94,15 @@ menuItems.click(function(e) {
 // Bind to scroll
 $(window).scroll(function() {
   // Get container scroll position
-  var fromTop = $(this).scrollTop() + topMenuHeight;
+  let fromTop = $(this).scrollTop() + topMenuHeight;
 
   // Get id of current scroll item
-  var cur = scrollItems.map(function() {
+  let cur = scrollItems.map(function() {
     if ($(this).offset().top < fromTop) return this;
   });
   // Get the id of the current element
   cur = cur[cur.length - 1];
-  var id = cur && cur.length ? cur[0].id : "";
+  let id = cur && cur.length ? cur[0].id : "";
 
   if (lastId !== id) {
     lastId = id;
@@ -115,4 +115,20 @@ $(window).scroll(function() {
       .parent()
       .addClass("current");
   }
+});
+
+//button back to top
+// Check distance to top and display back-to-top.
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 300) {
+    $(".back-to-top").addClass("show-back-to-top");
+  } else {
+    $(".back-to-top").removeClass("show-back-to-top");
+  }
+});
+
+// Click event to scroll to top.
+$(".back-to-top").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, 800);
+  return false;
 });
